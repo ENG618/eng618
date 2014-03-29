f28a620dea46daaf07c9cf148d454333c0f5f608
eng618 <eng618@fullsail.edu>
2014-03-29 00:32:34 -0400

    Added jQuery to site, attempted to add scroll to to

---
+++ js/main.js
@@ -0,0 +1,23 @@
+alert("js is loding correctly!");
+
+if (window.jQuery){
+	alert("jQuery is loading!! :)");
+}else{
+	alert("jQuery is not loding :(");
+}
+
+// scroll-to-top button show and hide
+jQuery(document).ready(function(){
+    jQuery(window).scroll(function(){
+        if (jQuery(this).scrollTop() > 100) {
+            jQuery('.scrollup').fadeIn();
+        } else {
+            jQuery('.scrollup').fadeOut();
+    }
+});
+// scroll-to-top animate
+jQuery('.scrollup').click(function(){
+    jQuery("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
