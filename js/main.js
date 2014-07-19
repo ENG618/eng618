3e909e6c52bc757f73953102ae479a9bb02807a2
eng618 <eng618@fullsail.edu>
2014-07-19 11:13:24 -0400

    Added contact.html and linked accordingly

--- js/main.js
+++ js/main.js
@@ -28,10 +28,4 @@ $('.scrollup').click(function(){
         return false;
     });
 
-// Google+ widget
-(function() {
-  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-  po.src = 'https://apis.google.com/js/platform.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
 });
